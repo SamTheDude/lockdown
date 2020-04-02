98e3764c3a389fc415d73d214520dcde63789d4b
SamTheDude <kent_sam@outlook.com>
2020-04-02 21:13:41 +0100

    Added rotating cube of fun.

--- js/animation-handler.js
+++ js/animation-handler.js
@@ -5,3 +5,25 @@ var renderer = new THREE.WebGLRenderer();
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+camera.position.y = 1;
+
+function animate() {
+    requestAnimationFrame( animate );
+    
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.001;
+
+	renderer.render( scene, camera );
+}
+
+
+
+animate();
