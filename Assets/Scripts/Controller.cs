9394feb54e542eb7c7c64fe83005a67ac84a63ea
LAPTOP-2EGHMQBN\toby <toby@thewrides.com>
2020-04-04 15:14:44 +0100

    Basic map created. Started implementing moving user's view of simulation

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private UserView cameraObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraObject = new UserView("Main Camera");
+        Debug.Log(cameraObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("Rotate right");
+            cameraObject.rotate(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("Rotate left");
+            cameraObject.rotate(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Move right");
+            cameraObject.move(1);
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Move left");
+            cameraObject.move(-1);
+        }
+        else if(Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Zoom in");
+            cameraObject.zoom(1);
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("Zoom out");
+            cameraObject.zoom(-1);
+        }
+    }
+}
---
+++ Assets/Scripts/UserView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserView
+{
+    private Transform cameraTransform;
+    private Vector3 position;
+
+    public UserView(string cameraName)
+    {
+        cameraTransform = GameObject.Find(cameraName).GetComponent<Transform>();
+        position = cameraTransform.position;
+        Debug.Log("(" + position.x + "," + position.y + "," + position.z + ")");
+    }
+
+    public void move(int direction)
+    {
+        float movedBy = direction /* Time.deltaTime*/;
+        cameraTransform.Translate(movedBy,0,0);
+        position.x += movedBy;
+        Debug.Log("(" + position.x + "," + position.y + "," + position.z + ")");
+    }
+
+    public void rotate(int direction)
+    {
+        cameraTransform.Rotate(0, direction /* Time.deltaTime*/, 0,Space.Self);
+    }
+
+    public void zoom(int direction)
+    {
+        float movedBy = direction /* Time.deltaTime*/;
+        cameraTransform.Translate(0, movedBy, 0);
+        position.y += movedBy;
+        Debug.Log("(" + position.x + "," + position.y + "," + position.z + ")");
+    }
+}
